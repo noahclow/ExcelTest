b42f3e95a3c22d5f31a8aa0813add5dd6fb369b7
Jesus Hernandez <noahclow@icloud.com>
2022-01-25 17:15:16 -0600

    Leer datos de excel

--- src/main/java/com/hexaware/Excel.java
+++ src/main/java/com/hexaware/Excel.java
@@ -6,19 +6,18 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
-public class Excel
-{
-    public static void main(String[] args)
-    {
-        crearExcel();
+public class Excel {
+    public static void main(String[] args) {
+        leerExcel();
     }
 
-    public static void  crearExcel(){
+    public static void crearExcel() {
         //Se crea la libro
         XSSFWorkbook workbook = new XSSFWorkbook();
 
@@ -27,11 +26,11 @@ public class Excel
 
         //Los datos a escribir estan en este mapa
         Map<String, Object[]> registro = new TreeMap<String, Object[]>();
-        registro.put("0", new Object[] {"NAME", "LASTNAME", "EMAIL", "PASSWORD", "COMPANY", "ADDRESS", "CITY", "ZIP_CODE", "MOBILE_PHONE"});
-        registro.put("1", new Object[] {"SomeName", "SomeLastName", "SomeEmail", "SomePassword.", "SomeCompany", "SomeAddress", "SomeCity", "SomePostCode", "SomeMobilePhone"});
-        registro.put("2", new Object[] {"testName", "testLastName", "testlead@test.io", "testPsw1234.", "testCompany", "testStreet", "testCity", 99999, 1234567890});
-        registro.put("3", new Object[] {"testName", "testLastName", "testlead@test.io", "testPsw1234.", "testCompany", "testStreet", "testCity", 99999, 1234567890});
-        registro.put("4", new Object[] {"testName", "testLastName", "testlead@test.io", "testPsw1234.", "testCompany", "testStreet", "testCity", 99999, 1234567890});
+        registro.put("0", new Object[]{"NAME", "LASTNAME", "EMAIL", "PASSWORD", "COMPANY", "ADDRESS", "CITY", "ZIP_CODE", "MOBILE_PHONE"});
+        registro.put("1", new Object[]{"SomeName", "SomeLastName", "SomeEmail", "SomePassword.", "SomeCompany", "SomeAddress", "SomeCity", "SomePostCode", "SomeMobilePhone"});
+        registro.put("2", new Object[]{"testName", "testLastName", "testlead@test.io", "testPsw1234.", "testCompany", "testStreet", "testCity", 99999, 1234567890});
+        registro.put("3", new Object[]{"testName", "testLastName", "testlead@test.io", "testPsw1234.", "testCompany", "testStreet", "testCity", 99999, 1234567890});
+        registro.put("4", new Object[]{"testName", "testLastName", "testlead@test.io", "testPsw1234.", "testCompany", "testStreet", "testCity", 99999, 1234567890});
 
         //Aqui se recorre el mapa para escribir los datos en la hoja
         Set<String> valor = registro.keySet();
@@ -39,34 +38,63 @@ public class Excel
         for (String llave : valor) // como se esta trabajando con un array se usa un for-each, para recorrer cada registro en el arreglo
         {
             Row fila = sheet.createRow(numeroDeFila++); // Aqui se escribe la fila y se incrementa el valor en 1
-            Object [] objArr = registro.get(llave);
+            Object[] objArr = registro.get(llave);
             int numeroDeColumna = 0;        // se puede indicar el numero de columna desde la que comenzara a escribir en la hoja
-            for (Object objeto : objArr)
-            {
+            for (Object objeto : objArr) {
                 Cell cell = fila.createCell(numeroDeColumna++);
-                if(objeto instanceof String)
-                {
-                    cell.setCellValue((String)objeto);
-                }
-                else if(objeto instanceof Integer)
-                {
-                    cell.setCellValue((Integer)objeto);
+                if (objeto instanceof String) {
+                    cell.setCellValue((String) objeto);
+                } else if (objeto instanceof Integer) {
+                    cell.setCellValue((Integer) objeto);
                 }
             }
         }
-        try
-        {
+        try {
             //Write the workbook in file system
             FileOutputStream out = new FileOutputStream(new File("Directory.xlsx"));
             workbook.write(out);
             out.close();
             System.out.println("Directory.xlsx written successfully on disk.");
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    
+    public static void leerExcel() {
+        try {
+            FileInputStream file = new FileInputStream(new File("Directory.xlsx"));
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            int numFilas = sheet.getLastRowNum();
+
+            for(int a = 0; a <= numFilas; a++){
+                Row fila = sheet.getRow(a);
+                int numCols = fila.getLastCellNum();
+
+                for(int b = 0; b < numCols; b++){
+                    Cell celda = fila.getCell(b);
+
+                    switch (celda.getCellType().toString()){
+                        case "NUMERIC":
+                            System.out.print(celda.getNumericCellValue() + " ");
+                            break;
+                        case "STRING":
+                            System.out.print(celda.getStringCellValue() + " ");
+                            break;
+                        case "FORMULA":
+                            System.out.print(celda.getCellFormula() + " ");
+                            break;
+                    }
+                }
+                System.out.println(" ");
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
